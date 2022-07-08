097ab694a667e7cf2c7e34f1839af0ae8a7053d3
Daniel Khalil <dkses.hef@uea.edu.br>
2022-07-08 17:47:34 -0400

    Criação do projeto de lista de contatos utilizando CRUD na lista de contatos

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,14 @@
+//Dados mockados
+
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1', name: 'Taylor', email: 'taylor@alunos.com.br', avatarUrl: ''),
+  '2': const User(
+      id: '2', name: 'Robyn', email: 'robyn@gmail.com', avatarUrl: ''),
+  '3': const User(
+      id: '3', name: 'Eilish', email: 'beilish@yahoo.com', avatarUrl: ''),
+  '4': const User(
+      id: '4', name: 'Rae', email: 'craejepsen@hotmail.com', avatarUrl: ''),
+};
