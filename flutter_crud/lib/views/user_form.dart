39282699b108e692b4a5b8bf14fa24610e084348
Daniel Khalil <dkses.hef@uea.edu.br>
2022-07-25 23:02:21 -0400

    Função de editar dados dos contatos

--- flutter_crud/lib/views/user_form.dart
+++ flutter_crud/lib/views/user_form.dart
@@ -7,8 +7,20 @@ class UserForm extends StatelessWidget {
   final _form = GlobalKey<FormState>();
   final Map<String, String> _formData = {};
 
+  void _loadFormData(User user) {
+    if (user == null) {
+      _formData['id'] = user.id;
+      _formData['name'] = user.name;
+      _formData['email'] = user.email;
+      _formData['avatarUrl'] = user.avatarUrl;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final user = ModalRoute.of(context)!.settings.arguments as User;
+    _loadFormData(user);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Usuário'),
@@ -23,10 +35,10 @@ class UserForm extends StatelessWidget {
 
                 Provider.of<Users>(context, listen: false).put(
                   User(
-                    id: _formData['id'],
-                    name: _formData['name'],
-                    email: _formData['email'],
-                    avatarUrl: _formData['avatarUrl'],
+                    id: _formData['id'] ?? '',
+                    name: _formData['name'] ?? '',
+                    email: _formData['email'] ?? '',
+                    avatarUrl: _formData['avatarUrl'] ?? '',
                   ),
                 );
 
@@ -43,6 +55,7 @@ class UserForm extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 TextFormField(
+                  initialValue: _formData['name'],
                   decoration: InputDecoration(labelText: 'Nome'),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -58,6 +71,7 @@ class UserForm extends StatelessWidget {
                   onSaved: (value) => _formData['name'] = value!,
                 ),
                 TextFormField(
+                    initialValue: _formData['email'],
                     decoration: InputDecoration(labelText: 'Email'),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
@@ -72,6 +86,7 @@ class UserForm extends StatelessWidget {
                     },
                     onSaved: (value) => _formData['email'] = value!),
                 TextFormField(
+                  initialValue: _formData['avatarUrl'],
                   decoration: InputDecoration(labelText: 'Url do Avatar'),
                   onSaved: (value) => _formData['avatarUrl'] = value!,
                 ),
